280560f6c9d9719f55f9e20957fdbad1ab554638
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-12 11:23:27 +0100

    no message

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::middleware(['auth', 'role:super admin|admin'])->name('admin.')->prefix('a
         Route::resource('bar-restaurant', AdminBarRestaurantPageController::class);
        /* Route::resource('rooms-page', AdminRoomsPageController::class);*/
         Route::resource('contact-us', AdminContactUsPageController::class);
-        Route::resource('rooms', AdminRoomsPageController::class);
+
         Route::resource('policy-pages', AdminPolicyPagesController::class);
     });
 });
