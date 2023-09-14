2fb1085e9df50ceb1317a330951672a825a8448e
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-14 10:34:04 +0100

    Our history page content update

--- resources/views/frontend/pages/aboutUs.blade.php
+++ resources/views/frontend/pages/aboutUs.blade.php
@@ -9,7 +9,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2">
-                        <h1>{{ $apc->hero_banner_title }}</h1>
+                        <h1>Our History</h1>
                         <p>Some content here</p>
                     </div>
                 </div>
@@ -20,27 +20,12 @@
     <section id="aboutPageBannerOne">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6">
+                <div class="col-md-6 wow slideInLeft" data-wow-duration="1s">
                     <h2>{{ $apc->about_banner_title }}</h2>
                     {!! $apc->about_banner_content !!}
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 wow slideInRight" data-wow-duration="1s">
                     <img class="img-fluid mainImg" src="{{ Storage::url($apc->about_banner_image) }}">
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="aboutPageBannerTwo">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <img class="img-fluid mainImg" src="{{ asset('images/our-history/banner-image.jpg') }}">
-                </div>
-                <div class="col-md-6">
-                    <h2>Some Title Here</h2>
-                    <p>
-                        Content here
-                    </p>
                 </div>
             </div>
         </div>
@@ -48,14 +33,28 @@
     <section id="aboutPageBannerThree">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6">
+                <div class="col-md-6 wow slideInLeft" data-wow-duration="1s">
+                    <img class="img-fluid mainImg" src="{{ Storage::url($apc->banner_two_image) }}">
+                </div>
+                <div class="col-md-6 wow slideInRight" data-wow-duration="1s">
                     <h3>
                         {{ $apc->banner_two_title }}
                     </h3>
-                    {!! $apc->banner_two_content !!}
-                </div>
-                <div class="col-md-6">
-                    <img class="img-fluid mainImg" src="{{ Storage::url($apc->banner_two_image) }}">
+                    <p>
+                        We can advise guests on how to book fishing on the River Spey or shooting on the nearby estates.  Storage for rods, cycles etc is available.
+                    </p>
+                    <p>
+                        The Speyside Way, a popular walking and cycling route is only a few moments away.
+                    </p>
+                    <p>
+                        Around the local area you will find the world renowned Walkers Shortbread bakery, as well as the Aberlour Distillery, both well worth a visit.  Many of the local distilleries have visitors centres and shops and offer tours where you can sample some of Scotlands finest whisky.  For the more adventurous among you, there is plenty of sport to be had, such as clay pegeon shooting, horse back riding and canoeing, as well as skiing and snowboarding on the cairngorms.
+                    </p>
+                    <p>
+                        For those looking to take a more gentle meander, there is plenty of sight seeing to be had, visit the Craigllachie Bridge, constructed in 1814 or take a visit to Knockando Woolmill.  No visit to Scotland would be complete wihtout a stop off at a castle, and there are plenty around to visit, Ballindalloch Castle from the sixteenth century is certainly a sight to behold.
+                    </p>
+                    <p>
+                        Don't forget to take a walk around Aberlour as the streets are adorned with local cafe's, bakeries, arts, crafts and gift shops.
+                    </p>
                 </div>
             </div>
         </div>
