78a0c9cbad47dcbe57d997e620ba1dae292f25ad
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-14 11:18:18 +0100

    Added trip advisor rating to the topbar

--- resources/views/frontend/layouts/partials/topBar.blade.php
+++ resources/views/frontend/layouts/partials/topBar.blade.php
@@ -1,13 +1,48 @@
 <div class="topBar">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-lg-6">
+            <div class="col-md-8 col-lg-4">
                 <ul class="list-inline contactDetails">
                     <li class="list-inline-item"><a href="tel:+441340881771">01340 881771</a></li>
                     <li class="list-inline-item"><a href="mailto:reservations@mashtun-aberlour.com">reservations@mashtun-aberlour.com</a></li>
                 </ul>
             </div>
-            <div class="col-md-4 col-lg-6">
+            <div class="col-lg-5">
+                <div class="tripAdviserBanner">
+                    <ul class="list-inline starRating">
+                        <li class="list-inline-item">
+                            <i class="fa-solid fa-star"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fa-solid fa-star"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fa-solid fa-star"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fa-solid fa-star"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <i class="fa-solid fa-star-half"></i>
+                        </li>
+                        <li class="list-inline-item">
+                            <span class="text">
+                                317 reviews
+                            </span>
+                        </li>
+                        <li class="list-inline-item">
+                            <span class="sep"> | </span>
+                        </li>
+                        <li class="list-inline-item">
+                            <p>
+                                #2 of 2 hotels in Aberlour
+                            </p>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-3">
                 <ul class="list-inline socialLinks">
                     <li class="list-inline-item"><a href="https://www.tripadvisor.co.uk/Hotel_Review-g658240-d672367-Reviews-The_Mash_Tun-Aberlour_Moray_Scotland.html" rel="nofollow" target="_blank"><img class="img-fluid tripAdvisorLogo" src="{{ asset('logos/trip-advisor.svg') }}" alt="TripAdvisor Logo"></a></li>
                     <li class="list-inline-item"><a href="https://www.facebook.com/themashtunaberlour/?locale=en_GB" rel="nofollow" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
