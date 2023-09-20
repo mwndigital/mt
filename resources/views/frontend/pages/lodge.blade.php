3373597695a6c0f49c4041ac6b9feab4bdde5d7b
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-20 11:02:38 +0100

    Lodge page update

--- resources/views/frontend/pages/lodge.blade.php
+++ resources/views/frontend/pages/lodge.blade.php
@@ -24,7 +24,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2">
-                        <h1>Our Lodge</h1>
+                        <h1>Stalla Dhu Lodge</h1>
                         <p>
                             Our 4 bedroom lodge with a luxury log cabin feel offers sumptuous relaxing options with sun deck, views of river to leathery embrace of gathering room. It is rented as a single rental and can accommodate 2-8 persons.
                         </p>
@@ -39,10 +39,13 @@
             <div class="row">
                 <div class="col-md-6">
                     <h2 class="secTitle">
-                        Indulge in a little piece of heaven
+                        Indulge in a little piece of heaven...
                     </h2>
                     <p>
-                        Nunc a hendrerit nisi, eu eleifend ligula. In quis mattis nisi. Donec aliquam tempor imperdiet. Ut sodales tincidunt tortor, vel commodo risus finibus vitae. Phasellus placerat leo id metus semper pharetra. Curabitur vehicula est eu erat lobortis, eget eleifend purus fringilla. Nullam feugiat tellus et efficitur vestibulum. Quisque quam nulla, imperdiet in feugiat vel, gravida vitae turpis. Quisque porttitor laoreet diam. Donec ut neque dignissim massa mollis imperdiet sed sit amet sapien. Vestibulum consequat ex cursus est maximus gravida. Nam id bibendum elit.
+                        The Stalla Dhu Lodge at the Mash Tun presents an inspiring getaway in the heart of Speyside with four luxury bedrooms and bathroom, it can accomodate 2 to 8 persons.
+                    </p>
+                    <p>
+                        You would be forgiven if you felt you were in The Rocky Mountains as it’s cozy, warm wood finishes meander thru the property with room names like Outlook and Hideout enhancing the welcome you will no doubt feel.
                     </p>
 
                 </div>
@@ -61,10 +64,13 @@
                 </div>
                 <div class="col-md-6">
                     <h2 class="secTitle">
-                        Indulge in a little piece of heaven
+                        Relax after a day of adventure...
                     </h2>
                     <p>
-                        Nunc a hendrerit nisi, eu eleifend ligula. In quis mattis nisi. Donec aliquam tempor imperdiet. Ut sodales tincidunt tortor, vel commodo risus finibus vitae. Phasellus placerat leo id metus semper pharetra. Curabitur vehicula est eu erat lobortis, eget eleifend purus fringilla. Nullam feugiat tellus et efficitur vestibulum. Quisque quam nulla, imperdiet in feugiat vel, gravida vitae turpis. Quisque porttitor laoreet diam. Donec ut neque dignissim massa mollis imperdiet sed sit amet sapien. Vestibulum consequat ex cursus est maximus gravida. Nam id bibendum elit.
+                        Slipper bath tubs enhance some of the rooms for that real prairie feeling.
+                    </p>
+                    <p>
+                        The Gathering Room with its rich leather furniture ,fireplace and dining tables will be your relaxing space after a day of Speyside adventure... private dining can all be arranged as can inspiring whisky tastings .
                     </p>
 
                 </div>
@@ -76,10 +82,16 @@
             <div class="row">
                 <div class="col-md-6">
                     <h3 class="secTitle">
-                        The nature of outside, whilst being inside
+                        Take a visit to our nurturing wood burning sauna....
                     </h3>
                     <p>
-                        Nunc a hendrerit nisi, eu eleifend ligula. In quis mattis nisi. Donec aliquam tempor imperdiet. Ut sodales tincidunt tortor, vel commodo risus finibus vitae. Phasellus placerat leo id metus semper pharetra. Curabitur vehicula est eu erat lobortis, eget eleifend purus fringilla. Nullam feugiat tellus et efficitur vestibulum. Quisque quam nulla, imperdiet in feugiat vel, gravida vitae turpis. Quisque porttitor laoreet diam. Donec ut neque dignissim massa mollis imperdiet sed sit amet sapien. Vestibulum consequat ex cursus est maximus gravida. Nam id bibendum elit.
+                        We invite you to experience all we have to offer from a relaxing drinks and dine deck with river view... or nurturing a wood burning sauna under the trees... we are at your service
+                    </p>
+                    <p>
+                        Stalla Dhu has a reserved parking spot and full use of the wood burning Sauna
+                    </p>
+                    <p>
+                        Plan soon for adventure within an adventure...
                     </p>
 
                 </div>
