bf8c9de220b1fc290fdb6035b15cc6b255de3c07
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-21 15:33:33 +0100

    New testimonials added.

--- resources/views/frontend/pages/partials/testimonialsBanner.blade.php
+++ resources/views/frontend/pages/partials/testimonialsBanner.blade.php
@@ -6,6 +6,38 @@
                 <div class="owl-carousel testimonialMainCarousel">
 
                     <div class="item">
+                        <h5>Scotch Treat</h5>
+                        <p>Recently retired, visiting Scotch whiskey country we discovered this intimate retreat. The setting is beautiful in the bucolic Scottish countryside. The staff was attentive and accommodating. The food was outstanding. I highly recommend this one of a kind location experience.</p>
+                        <h6>September 2023</h6>
+                    </div>
+                    <div class="item">
+                        <h5>Perfect Night</h5>
+                        <p>
+                            This place is a gem. We had a wonderful evening enjoying our own selfmade whiskey tasting. Very friendly staff and as far as we heard perfect food as well. If you stay or travel trough - don't miss the Mash Tun 👍🏻
+                        </p>
+                        <h6>
+                            June 2023
+                        </h6>
+                    </div>
+                    <div class="item">
+                        <h5>Hidden away but a gem</h5>
+                        <p>
+                            Just on the side of the river is this gem, hidden away from the Main Street serving a 1st class evening meal & an even better breakfast, take a walk up to the main road, turn right & there’s Aberlour distillery, turn left for 10 minutes & you’ve got Walkers biscuits, a great find.
+                        </p>
+                        <h6>
+                            March 2023
+                        </h6>
+                    </div>
+                    <div class="item">
+                        <h5>Amazing place, centrally located in whisky country!</h5>
+                        <p>
+                            The staff and place is amazing. Food in restaurant (both breakfast and dinner) was excellent and the staff looked after everything we could imagine. British Airways lost our luggage and they even offered to do our laundry!!! We will definitely stay here again as we loved the experience! All staff were incredible, but Sharon the manager was so friendly and attentive.
+                        </p>
+                        <h6>
+                            December 2022
+                        </h6>
+                    </div>
+                    <div class="item">
                         <h5>THIS IS A GEM</h5>
                         <p>
                             The dinner was exceptional (make sure you book) the breakfast was great and the hotels setting very good. The bar has a wonderful selection of whiskies - we had a great evening
