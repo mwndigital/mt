ac61eb09d70262955b6e45449e9f8476eb829db7
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-22 14:54:33 +0100

    Admin side table booking index view created and assets built

--- resources/views/admin/pages/restaurant/index.blade.php
+++ resources/views/admin/pages/restaurant/index.blade.php
@@ -30,6 +30,146 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
+                    <div class="tab-panels-wrap">
+                        <div class="list-group" id="list-tab" role="tablist">
+                            <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-today" role="tab">Todays Bookings</a>
+                            <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-this-week" role="tab">This Weeks Bookings</a>
+                            <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">All Bookings</a>
+                        </div>
+
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="list-today" role="tabpanel">
+                                <table class="table table-responsive w-100">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Joining for</th>
+                                            <th>Time</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($todaysBookings as $booking)
+                                            <tr>
+                                                <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
+                                                <td>{{ $booking->joining_for }}</td>
+                                                <td>{{ $booking->reservation_time }}</td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-end">
+                                                            <ul>
+
+                                                                <li>
+                                                                    <a href="">Edit</a>
+                                                                </li>
+                                                                <li>
+                                                                    <form action="" method="POST">
+                                                                        @csrf
+                                                                        @method('delete')
+                                                                        <button class="confirm-delete-btn" type="submit">Delete</button>
+                                                                    </form>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane fade" id="list-this-week" role="tabpanel">
+                                <table class="table table-responsive w-100">
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Joining for</th>
+                                        <th>Time</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($thisWeeksBookings as $booking)
+                                        <tr>
+                                            <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
+                                            <td>{{ $booking->joining_for }}</td>
+                                            <td>{{ $booking->reservation_time }}</td>
+                                            <td>
+                                                <div class="dropdown">
+                                                    <button class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                                                    </button>
+                                                    <div class="dropdown-menu dropdown-menu-end">
+                                                        <ul>
+
+                                                            <li>
+                                                                <a href="">Edit</a>
+                                                            </li>
+                                                            <li>
+                                                                <form action="" method="POST">
+                                                                    @csrf
+                                                                    @method('delete')
+                                                                    <button class="confirm-delete-btn" type="submit">Delete</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="tab-pane fade" id="list-messages" role="tabpanel">
+                                <table class="table table-responsive w-100">
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Joining for</th>
+                                        <th>Time</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($allBookings as $booking)
+                                        <tr>
+                                            <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
+                                            <td>{{ $booking->joining_for }}</td>
+                                            <td>{{ $booking->reservation_time }}</td>
+                                            <td>
+                                                <div class="dropdown">
+                                                    <button class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                                                    </button>
+                                                    <div class="dropdown-menu dropdown-menu-end">
+                                                        <ul>
+
+                                                            <li>
+                                                                <a href="">Edit</a>
+                                                            </li>
+                                                            <li>
+                                                                <form action="" method="POST">
+                                                                    @csrf
+                                                                    @method('delete')
+                                                                    <button class="confirm-delete-btn" type="submit">Delete</button>
+                                                                </form>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
                     <table class="table table-hovered">
 
                     </table>
