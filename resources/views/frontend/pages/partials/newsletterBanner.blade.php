c03ed0c854f46a3b19143fdb781097719d816088
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-27 12:34:52 +0100

    Updated the newsletter form on FE

--- resources/views/frontend/pages/partials/newsletterBanner.blade.php
+++ resources/views/frontend/pages/partials/newsletterBanner.blade.php
@@ -4,32 +4,28 @@
             <div class="col-md-8 offset-md-2">
                 <h5>Sign up to our newsletter</h5>
                 <p>Want to keep up to date with everything going on at The Mash Tun, sign up to our newsletter below!</p>
-                <form action="">
+                <form action="https://cgarsltd.us14.list-manage.com/subscribe/post" method="POST">
+                    <input type="hidden" name="ht" value="28ebbbd1eec9a492c8692d51f6fd348d427d2433:MTY5NTgxMzcwNi42MDcy">
+                    <input type="hidden" name="mc_signupsource" value="hosted">
                     <div class="row">
-                        <div class="col-12">
-                            <label for="">Full Name</label>
-                            <input type="text" name="full_name" id="full_name">
+                        <div class="col-md-6">
+                            <label for="">First Name</label>
+                            <input type="text" name="MERGE1" id="MERGE1" size="25" value="">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="">Last Name</label>
+                            <input type="text" name="MERGE2" id="MERGE2" size="25" value="">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12">
                             <label for="">Email Address</label>
-                            <input type="email" name="email" id="email">
+                            <input type="email" autocapitalize="none" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input type="checkbox" name="signup_accept_checkbox" id="signup_accept_checkbox" class="form-check-input">
-                                <label class="form-check-label">
-                                    I would like to keep up to date on everything going on at The Mash Tun, including news, offers and upcoming events.
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <button type="submit" class="blueOutlineBtn">Subscribe</button>
+                        <div class="col-12 d-flex justify-content-end">
+                            <input type="submit" class="formEmailButton" name="submit" value="Subscribe">
                         </div>
                     </div>
                 </form>
