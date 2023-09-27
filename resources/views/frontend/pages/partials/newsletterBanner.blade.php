7d8f16199ab3b5f302ed5aeb98e7373020ef9f43
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-27 13:11:53 +0100

    Updated newsletter signup banner to work with MailChimp, also added in custom styles for the responses too

--- resources/views/frontend/pages/partials/newsletterBanner.blade.php
+++ resources/views/frontend/pages/partials/newsletterBanner.blade.php
@@ -4,32 +4,41 @@
             <div class="col-md-8 offset-md-2">
                 <h5>Sign up to our newsletter</h5>
                 <p>Want to keep up to date with everything going on at The Mash Tun, sign up to our newsletter below!</p>
-                <form action="https://cgarsltd.us14.list-manage.com/subscribe/post" method="POST">
+                <form action="https://cgarsltd.us14.list-manage.com/subscribe/post?u=ba2d8cb9665d1d5270b9840bb&amp;id=ef33d2f324&amp;f_id=00ffc2e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
                     <input type="hidden" name="ht" value="28ebbbd1eec9a492c8692d51f6fd348d427d2433:MTY5NTgxMzcwNi42MDcy">
                     <input type="hidden" name="mc_signupsource" value="hosted">
                     <div class="row">
                         <div class="col-md-6">
                             <label for="">First Name</label>
-                            <input type="text" name="MERGE1" id="MERGE1" size="25" value="">
+                            <input type="text" name="FNAME" class=" text" id="mce-FNAME" value="">
                         </div>
                         <div class="col-md-6">
                             <label for="">Last Name</label>
-                            <input type="text" name="MERGE2" id="MERGE2" size="25" value="">
+                            <input type="text" name="LNAME" class=" text" id="mce-LNAME" value="">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12">
                             <label for="">Email Address</label>
-                            <input type="email" autocapitalize="none" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="">
+                            <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12 d-flex justify-content-end">
-                            <input type="submit" class="formEmailButton" name="submit" value="Subscribe">
+                            <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
                         </div>
                     </div>
+                    <div id="mce-responses" class="clear">
+                        <div class="response alert alert-danger mt-4" role="alert" id="mce-error-response" style="display: none;"></div>
+                        <div class="response alert alert-success mt-4" role="alert" id="mce-success-response" style="display: none;"></div>
+                    </div>
+                    <div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_ba2d8cb9665d1d5270b9840bb_ef33d2f324" tabindex="-1" value=""></div>
+                    <div class="clear"></div>
                 </form>
             </div>
         </div>
     </div>
 </section>
+
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
