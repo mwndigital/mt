b5b51a0c7bab4254d1ea2edd6734bd81fdab3688
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-09-29 17:32:00 +0100

    Migration to amend banner one content col

---
+++ database/migrations/2023_09_29_163051_remove_col_from_bar_page_content_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bar_page_content', function (Blueprint $table) {
+            $table->dropColumn('banner_one_content');
+            $table->text('banner_one_content')->after('banner_one_title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bar_page_content', function (Blueprint $table) {
+            //
+        });
+    }
+};
