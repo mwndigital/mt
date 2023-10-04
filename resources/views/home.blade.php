355251ca6322b245e3b97340c0b2aa6290dcb854
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-10-04 17:11:34 +0100

    Updated the default home view to do a redirect depending on the users role to stop any instances of a customer or admin going to a blank page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+@if(Auth()::user()->hasRole(['super admin', 'admin']))
+    <script>
+        window.location = '{{ route('admin.dashboard') }}';
+    </script>
+@elseif(Auth()::user()->hasRole('customer'))
+    <script>
+        window.location = '{{ route('customer.dashboard') }}';
+    </script>
+@else
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>
+                You do not belong here
+            </h1>
         </div>
     </div>
-</div>
-@endsection
+@endif
+
