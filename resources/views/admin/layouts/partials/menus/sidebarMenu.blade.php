824cbf8708cceb870eb92e5fd17f431a68dcbdb3
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-10-05 12:33:02 +0100

    Added in staff user seeder and also amended the sidebar to only display certain things for staff user based on the role

--- resources/views/admin/layouts/partials/menus/sidebarMenu.blade.php
+++ resources/views/admin/layouts/partials/menus/sidebarMenu.blade.php
@@ -2,30 +2,32 @@
     <li>
         <a href="{{ route('admin.dashboard') }}">Dashboard</a>
     </li>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Pages
-        </a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.homepage.edit', ['homepage' => 1]) }}">Homepage</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.about-us.edit', ['about_u' => 1]) }}">About us</a>
-            </li>
-            <li><a href="{{ route('admin.rooms-page.edit', ['rooms_page' => 1]) }}">The Rooms</a></li>
+    @role('super admin|admin')
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Pages
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.homepage.edit', ['homepage' => 1]) }}">Homepage</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.about-us.edit', ['about_u' => 1]) }}">About us</a>
+                </li>
+                <li><a href="{{ route('admin.rooms-page.edit', ['rooms_page' => 1]) }}">The Rooms</a></li>
 
-            <li>
-                <a href="{{ route('admin.faqs-page.edit', ['faqs_page' => 1]) }}">FAQs</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.bar-page.edit', ['bar_page' => 1]) }}">Bar</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.dining-page.edit', ['dining_page' => 1]) }}">Dining</a>
-            </li>
-        </ul>
-    </li>
+                <li>
+                    <a href="{{ route('admin.faqs-page.edit', ['faqs_page' => 1]) }}">FAQs</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.bar-page.edit', ['bar_page' => 1]) }}">Bar</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.dining-page.edit', ['dining_page' => 1]) }}">Dining</a>
+                </li>
+            </ul>
+        </li>
+    @endrole
     <li>
         <a href="{{ route('admin.bookings.index') }}">Room Bookings</a>
     </li>
@@ -39,6 +41,9 @@
                 <a href="{{ route('admin.restaurant-bookings.create') }}">Create Booking</a>
             </li>
             <li>
+                <a href="{{ route('admin.restaurant-bookings.csv-upload') }}">Upload CSV for Bookings</a>
+            </li>
+            <li>
                 <a href="{{ route('admin.restaurant-tables.index') }}">All Tables</a>
             </li>
             <li>
@@ -46,84 +51,96 @@
             </li>
         </ul>
     </li>
-    <li><a href="/admin/rooms">Rooms</a></li>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gallery</a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.gallery.index') }}">All Gallery Items</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.gallery.create') }}">Create Gallery Item</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.gallery-category.index') }}">All Gallery Categories</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.gallery-category.create') }}">Create New Gallery Category</a>
-            </li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.menu.index') }}">All Menu Items</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.menu.create') }}">Create Menu Item</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.menu-category.index') }}">All Menu Categories</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.menu-category.create') }}">Create new menu category</a>
-            </li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Whisky Selection
-        </a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.whisky.index') }}">All Whisky Selection</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.whisky.create') }}">Add New Whisky</a>
-            </li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="falase">
-            FAQs
-        </a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.faq.index') }}">All FAQs</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.faq.create') }}">Add New FAQ</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.faq-category.index') }}">All FAQ Categories</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.faq-category.create') }}">Add new FAQ Category</a>
-            </li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Policy Pages
-        </a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('admin.policy-pages.index') }}">All Policy Pages</a>
-            </li>
-            <li><a href="{{ route('admin.policy-pages.create') }}">Create New Policy Page</a></li>
-        </ul>
-    </li>
-    <li><a href="">Settings</a></li>
+    @role('super admin|admin')
+        <li><a href="/admin/rooms">Rooms</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gallery</a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.gallery.index') }}">All Gallery Items</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.gallery.create') }}">Create Gallery Item</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.gallery-category.index') }}">All Gallery Categories</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.gallery-category.create') }}">Create New Gallery Category</a>
+                </li>
+            </ul>
+        </li>
+    @endrole
+    @role('super admin|admin')
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.menu.index') }}">All Menu Items</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.menu.create') }}">Create Menu Item</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.menu-category.index') }}">All Menu Categories</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.menu-category.create') }}">Create new menu category</a>
+                </li>
+            </ul>
+        </li>
+    @endrole
+    @role('super admin|admin')
+        <li class="dropdown">
+            <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Whisky Selection
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.whisky.index') }}">All Whisky Selection</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.whisky.create') }}">Add New Whisky</a>
+                </li>
+            </ul>
+        </li>
+    @endrole
+    @role('super admin|admin')
+        <li class="dropdown">
+            <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="falase">
+                FAQs
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.faq.index') }}">All FAQs</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.faq.create') }}">Add New FAQ</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.faq-category.index') }}">All FAQ Categories</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.faq-category.create') }}">Add new FAQ Category</a>
+                </li>
+            </ul>
+        </li>
+    @endrole
+    @role('super admin|admin')
+        <li class="dropdown">
+            <a href="" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Policy Pages
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="{{ route('admin.policy-pages.index') }}">All Policy Pages</a>
+                </li>
+                <li><a href="{{ route('admin.policy-pages.create') }}">Create New Policy Page</a></li>
+            </ul>
+        </li>
+    @endrole
+    @role('super admin|admin')
+        <li><a href="">Settings</a></li>
+    @endrole
 
 </ul>
