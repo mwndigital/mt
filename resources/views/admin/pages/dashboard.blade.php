24bfe501e96e54f6bb2c6e5f11df4b85b37984ef
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-10-20 15:14:40 +0100

    added two new tabs to the admin dashboard for room and restaurant bookings to display the latest bookings

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -23,13 +23,84 @@
                         <h2>Restaurant Bookings</h2>
                         <div class="tab-panels-wrap">
                             <div class="list-group" id="list-tab" role="tablist">
-                                <a class="list-group-item list-group-item-action active" data-bs-toggle="list"
+                                <a href="" class="list-group-item list-group-item-action active" data-bs-toggle="list" href='#restaurant-list-latest' role="tab">
+                                    Latest Bookings
+                                </a>
+                                <a class="list-group-item list-group-item-action" data-bs-toggle="list"
                                    href="#restaurant-list-today" role="tab">Todays Bookings</a>
                                 <a class="list-group-item list-group-item-action" data-bs-toggle="list"
                                    href="#restaurant-list-this-week" role="tab">This Weeks Bookings</a>
                             </div>
                             <div class="tab-content" id="nav-tabContent">
-                                <div class="tab-pane fade show active" id="restaurant-list-today" role="tabpanel">
+                                <div class="tab-pane fade show active" id="restaurant-list-latest" role="tabpanel">
+                                    <table class="table w-100">
+                                        <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Time</th>
+                                            <th>Date</th>
+                                            <th>Joining for</th>
+                                            <th>Booking made</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($newestRestaurantBookings as $today)
+                                            <tr>
+                                                <td>{{ $today->first_name }} {{ $today->last_name }}</td>
+                                                <td>{{ $today->reservation_time }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($today->reservation_date)) }}</td>
+                                                <td>{{ $today->joining_for }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($today->created_at)) }}</td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button class="dropdown-toggle" role="button"
+                                                                data-bs-toggle="dropdown" aria-expanded="false">
+                                                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-end">
+                                                            <ul>
+                                                                <li>
+                                                                    <a href="{{ route('admin.restaurant-bookings.show', $today->id) }}">
+                                                                        View
+                                                                    </a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="{{ route('admin.restaurant-bookings.edit', $today->id) }}">Edit</a>
+                                                                </li>
+                                                                <li>
+                                                                    <form
+                                                                        action="{{ route('admin.restaurant-bookings.cancel-booking', $today->id) }}"
+                                                                        method="POST">
+                                                                        @csrf
+                                                                        @method('PUT') <!-- Add this hidden field to override the method -->
+                                                                        <button type="submit" class="cancelBookingBtn">
+                                                                            Cancel Booking
+                                                                        </button>
+                                                                    </form>
+
+                                                                </li>
+                                                                <li>
+                                                                    <form
+                                                                        action="{{ route('admin.restaurant-bookings.destroy', $today->id) }}"
+                                                                        method="POST">
+                                                                        @csrf
+                                                                        @method('delete')
+                                                                        <button class="confirm-delete-btn"
+                                                                                type="submit">Delete
+                                                                        </button>
+                                                                    </form>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="tab-pane fade show" id="restaurant-list-today" role="tabpanel">
                                     <table class="table w-100">
                                         <thead>
                                         <tr>
@@ -98,6 +169,7 @@
                                         <thead>
                                         <tr>
                                             <th>Name</th>
+                                            <th>Date</th>
                                             <th>Time</th>
                                             <th>Joining for</th>
                                             <th>Actions</th>
@@ -107,6 +179,7 @@
                                         @foreach($restaurantThisWeek as $today)
                                             <tr>
                                                 <td>{{ $today->first_name }} {{ $today->last_name }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($today->reservation_date)) }}</td>
                                                 <td>{{ $today->reservation_time }}</td>
                                                 <td style="text-transform: uppercase;">{{ $today->joining_for }}</td>
                                                 <td>
@@ -166,13 +239,43 @@
                         <h2>Room Bookings</h2>
                         <div class="tab-panels-wrap">
                             <div class="list-group" id="list-tab" role="tablist">
-                                <a class="list-group-item list-group-item-action active" data-bs-toggle="list"
+                                <a class="list-group-item list-group-item-action active" data-bs-toggle="list" href="#room-list-latest" role="tab">Latest Bookings</a>
+                                <a class="list-group-item list-group-item-action" data-bs-toggle="list"
                                    href="#room-list-today" role="tab">Todays Bookings</a>
                                 <a class="list-group-item list-group-item-action" data-bs-toggle="list"
                                    href="#room-list-this-week" role="tab">This Weeks Bookings</a>
                             </div>
                             <div class="tab-content" id="nav-tabContent">
-                                <div class="tab-pane fade show active" id="room-list-today" role="tabpanel">
+                                <div class="tab-pane fade show active" id="room-list-latest" role="tabpanel">
+                                    <table class="table w-100">
+                                        <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Checkin Date & Checkout Date</th>
+                                            <th>Arrival Time</th>
+                                            <th>Room(s)</th>
+                                            <th>Booking made</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($roomToday as $today)
+                                            <tr>
+                                                <td>{{ $today->first_name }} {{ $today->last_name }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($today->checkin_date)) }}
+                                                    - {{ date('d/m/Y', strtotime($today->checkout_date)) }}</td>
+                                                <td>{{ $today->arrival_time }}</td>
+                                                <td style="text-align: right;">
+                                                    @foreach ($today->rooms as $room )
+                                                        {{ $room->name }}<br/>
+                                                    @endforeach
+                                                </td>
+                                                <td style="text-align: right;">{{ date('d/m/Y', strtotime($today->created_at)) }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="tab-pane fade show" id="room-list-today" role="tabpanel">
                                     <table class="table w-100">
                                         <thead>
                                         <tr>
