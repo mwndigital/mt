2362a3fb28257f68d9d2876d2dde72276c4b0e50
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-11-24 14:47:42 +0000

    Notices banner added, updated the menus menu item to work properly on mobile

---
+++ resources/views/frontend/layouts/partials/noticesBanner.blade.php
@@ -0,0 +1,11 @@
+<section class="noticesBanner">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <button type="button" data-bs-toggle="modal" data-bs-target="#viewChristmasMenuModal">
+                    Come visit us this festive season and enjoy our heart warming Christmas menu at Mash Tun… <i class="fa-solid fa-chevron-right"></i><i class="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
