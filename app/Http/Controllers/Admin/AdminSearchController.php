0d84c290a99f9263de3eb668a81ee6057a2f26ac
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2023-12-19 09:45:26 +0000

    Updated search controller to use UK dates

--- app/Http/Controllers/Admin/AdminSearchController.php
+++ app/Http/Controllers/Admin/AdminSearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
 use App\Models\RestaurantBooking;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminSearchController extends Controller
@@ -16,9 +17,31 @@ class AdminSearchController extends Controller
     {
         $query = $request->input('search');
 
-        $bookings = Booking::search($query)->get();
-        $restaurantBooking = RestaurantBooking::search($query)->get();
+        // Format date if it matches a specific pattern (e.g., 'd/m/Y')
+        if ($this->isDate($query)) {
+            $formattedDate = Carbon::createFromFormat('d/m/Y', $query)->format('Y-m-d');
+            $bookings = Booking::search($formattedDate)->get();
+            $restaurantBooking = RestaurantBooking::search($formattedDate)->get();
+        } else {
+            // Handle other types of queries or directly use the input query
+            $bookings = Booking::search($query)->get();
+            $restaurantBooking = RestaurantBooking::search($query)->get();
+        }
 
-        return view('admin.pages.search.results', compact( 'restaurantBooking', 'bookings'));
+        return view('admin.pages.search.results', compact('restaurantBooking', 'bookings'));
+    }
+
+    /**
+     * Check if the input is a date with the specified format.
+     *
+     * @param string $input
+     * @param string $format
+     * @return bool
+     */
+    private function isDate($input, $format = 'd/m/Y')
+    {
+        $parsedDate = Carbon::createFromFormat($format, $input);
+
+        return $parsedDate && $parsedDate->format($format) === $input;
     }
 }
