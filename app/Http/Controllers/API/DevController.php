1529373c3d5e8210a961f2db964aef212d92a3dd
agtt <csd@hotmail.com.tr>
2023-12-20 16:58:45 +0000

    Commented out Artisan commands for optimization and storage linking

--- app/Http/Controllers/API/DevController.php
+++ app/Http/Controllers/API/DevController.php
@@ -15,8 +15,8 @@ class DevController extends Controller
             shell_exec('/usr/local/cpanel/bin/uapi VersionControl update name=mt repository_root=/home/mashtunaberlour/repositories/mt branch=main source_repository=\'{"remote_name":"origin"}\'
         ');
             shell_exec('/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root=/home/mashtunaberlour/repositories/mt');
-            Artisan::call('optimize:clear');
-            Artisan::call('storage:link');
+            // Artisan::call('optimize:clear');
+            // Artisan::call('storage:link');
             echo "done";
         } catch (\Exception $e) {
             echo $e->getMessage();
